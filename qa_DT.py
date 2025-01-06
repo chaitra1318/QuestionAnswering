b245498083b87079e7e2f20a2129a3b66e613a9c
chaitra1318 <44636735+chaitra1318@users.noreply.github.com>
2025-01-06 17:19:24 +0530

    Add files via upload

---
+++ qa_DT.py
@@ -0,0 +1,55 @@
+import os
+import langchain
+import openai
+import warnings
+import sys
+
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.llms import OpenAI
+from langchain.retrievers.self_query.base import SelfQueryRetriever
+from langchain.chains.query_constructor.base import AttributeInfo
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
+import shutup
+shutup.please()
+
+#warnings.filterwarnings("ignore")
+warnings.filterwarnings("ignore", category=DeprecationWarning, module="langchain.chat_models")
+
+
+os.environ["OPENAI_API_KEY"] = ''
+
+
+#embedding = OpenAIEmbeddings()
+vectordb = Chroma(persist_directory="./db", embedding_function=OpenAIEmbeddings())
+
+retriever= vectordb.as_retriever(searchtype="mmr",k=3)
+
+model = "gpt-4o"
+#llm = OpenAI(model=model)
+llm = ChatOpenAI(model="gpt-4o")
+
+# Build prompt
+template = """
+From the retrieved text construct a discourse tree. Do not print the discourse tree
+Answer the query from the most relevant clause in the discourse tree and it cannot be more than 15 words. Provide 3 follow up questions based on the clauses
+in the discourse tree so that we would have answers for the follow up questions.{context}
+Question: {question}
+Helpful Answer: """
+QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm,
+    retriever=vectordb.as_retriever(),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
+)
+question =sys.argv[1]
+#print(question)
+result = qa_chain({"query": question})
+print(result["result"])
